34f272fe6cf6f8de189c2662580d50abfec418d6
mayhemphone <mayhemphone@gmail.com>
2019-03-14 19:20:31 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,23 @@
 console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne + " & " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+//check if two cards have been flipped
+if (cardsInPlay.length === 2){
+	console.log("User flipped a " + cardOne + ", and a " + cardTwo);
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	};
+	if (cardsInPlay[0] !== cardsInPlay[1]){
+		alert("Sorry, Try again!");
+	}
+	
+};
